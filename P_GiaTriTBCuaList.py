4bd67b19e2de307f499513eebba5206b234159cb
TamNhien <canhthien91@gmail.com>
2024-03-27 20:20:44 +0700

    Add project files.

---
+++ P_GiaTriTBCuaList.py
@@ -0,0 +1,4 @@
+﻿lst = list(map(int, input("Nhập danh sách số nguyên, cách nhau bởi dấu phẩy: ").split(',')))
+average = sum(lst) / len(lst) if lst else 0
+print("Giá trị trung bình của danh sách là : ", average)
+
